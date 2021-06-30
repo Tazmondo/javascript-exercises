5c12b9cb44f32731e4db6f51e618dc065f9539a5
Tazmondo <fondcoolman@outlook.com>
2021-06-30 02:14:00 +0100

    findTheOldest solution

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person) => {
+        if (oldestPerson === undefined){
+            oldestPerson = person;
+        } else {
+            let newAge = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+            let oldAge = (oldestPerson.yearOfDeath || new Date().getFullYear()) - oldestPerson.yearOfBirth;
+            oldestPerson = (newAge > oldAge ? person : oldestPerson);
+        }
+        return oldestPerson;
+    })
 };
 
 module.exports = findTheOldest;
